4449e2b147fb0295024c8cf9aad8eea444563e63
Hesham <hesham.ali.3959@gmail.com>
2024-10-24 00:25:12 +0300

    add selenium Grid to the project

--- src/test/java/tests/UserRegistrationTest.java
+++ src/test/java/tests/UserRegistrationTest.java
@@ -18,29 +18,28 @@ public class UserRegistrationTest extends TestBase{
     public void UserCanRegisterSuccessfully() throws InterruptedException {
         homePage=new HomePage(driver);
         registrationPage =new UserRegistrationPage(driver);
+
         //System.out.println(homePage.);
         //homePage.human();
         //homePage.openRegistrationPage();
-        registrationPage.userRegistration("Hesham","ALi","h22@gmail.com","12345678");
-        Assert.assertTrue(registrationPage.successMessage.getText().contains("Your registration completed"));
-        //Thread.sleep(5000);
 
-//        Thread.sleep(4000);
-//        driver.findElement(By.partialLinkText("Computers")).click();
-//        driver.findElement(By.partialLinkText("Software")).click();
+        registrationPage.userRegistration("Hesham","ALi","he9@gmail.com","12345678");
+        Assert.assertTrue(registrationPage.successMessage.getText().contains("Your registration completed"));
+
+
     }
-    @Test(priority = 2,dependsOnMethods = {"UserCanRegisterSuccessfully"})
+    @Test(priority = 2,dependsOnMethods = {"UserCanRegisterSuccessfully"},enabled = false)
     public void registerUserCanLogOut() throws InterruptedException {
         homePage.logOut();
         //Thread.sleep(5000);
     }
-    @Test(priority = 3)
+    @Test(priority = 3,enabled = false)
     public void registerUserCanLogin() throws InterruptedException {
         homePage=new HomePage(driver);
         loginPage=new LoginPage(driver);
         homePage.openLoginPage();
-        loginPage.login("he@gmail.com","123456");
-        System.out.println(homePage.logOutBtn.getText().contains("Log out"));
+        //loginPage.login("he7@gmail.com","123456");
+       // System.out.println(homePage.logOutBtn.getText().contains("Log out"));
         //Assert.assertTrue(homePage.logOutBtn.getText().contains("Log out"));
        // Thread.sleep(5000);
 
